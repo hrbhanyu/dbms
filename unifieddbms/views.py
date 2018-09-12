e570e1ec1b2a4fb09275b1d2ac648e7e6e7be5d7
hrbhanyu <48898944@qq.com>
2018-09-12 16:43:39 +0800

    add a index page to display server information

--- unifieddbms/views.py
+++ unifieddbms/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,render_to_response
+from django.http import HttpRequest
+from django import template
+from unifieddbms.models import *
 
 # Create your views here.
+def index(request):
+    result = host_info.objects.all().order_by('server_id')
+    dic = {'result':result}
+    return render_to_response('index.html',dic)
